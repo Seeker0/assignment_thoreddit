017dacc258d7d8ea7d426e92975dc6532165c4a9
Jeff Dederick <Xpressivmotion@gmail.com>
2017-12-06 18:30:05 -0500

    seeds altered

--- seeds/index.js
+++ seeds/index.js
@@ -32,7 +32,7 @@ mongooseeder.seed({
         title: 'Example Title' + i,
         content:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        childIds: [{ i }]
+        childId: { i }
       });
       posts.push(post);
     }
@@ -46,7 +46,7 @@ mongooseeder.seed({
         title: 'Example Comment' + i,
         content: 'This is a great comment!',
         parentId: i,
-        childIds: [{ childid }]
+        childIds: { childid }
       });
       comments.push(comment);
     }
